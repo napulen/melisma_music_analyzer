aeae6e71c7efc5bb9e89acda3a6af544c98ba356
Néstor Nápoles López <napulen@gmail.com>
2022-08-06 22:00:27 -0400

    Send stderr to devnull in run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bin/tsroot --meldir $MELISMA/bin --midir $HUMEXTRA/bin --roman $1 > /tmp/rna.krn
+bin/tsroot --meldir $MELISMA/bin --midir $HUMEXTRA/bin --roman $1 2>/dev/null > /tmp/rna.krn
 $HUMDRUM/bin/assemble $1 /tmp/rna.krn
